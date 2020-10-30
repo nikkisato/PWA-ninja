9f6f75c215419132c552e6bc834d19dd0a4619dc
Nikki <nikki.satopdx@gmail.com>
2020-10-29 20:54:20 -0700

    registered serviceWorker

---
+++ js/app.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then((reg) => {
+      console.log('SERVICE WORKER REGISTERED', reg);
+    })
+    .catch((err) => {
+      console.log('SERVICE WORKER NOT REGISTERED', err);
+    });
+}
